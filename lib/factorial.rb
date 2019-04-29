78f38a73005b975dc4226725293373e264c363d2
qqdipps <sdipp007@gmail.com>
2019-04-29 13:12:05 -0700

    added logic, tests pass for both implementations

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,20 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
-def factorial(number)
-  raise NotImplementedError
+# Time complexity: 0(n) where n is the size of the number factorial is called on.
+# Space complexity: 0(1)
+
+# unsure how to do time and space analysis on recursive functions.
+
+# def factorial(number)
+#   raise ArgumentError.new unless number
+#   number > 0 ? number * factorial(number - 1) : 1
+# end
+
+def factorial(n)
+  raise ArgumentError.new unless n
+  fact = 1
+  until n == 0
+    fact *= n
+    n -= 1
+  end
+  return fact
 end
